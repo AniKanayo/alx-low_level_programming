1ace9fb7dfb67ece03d9a509d23fdb41ed6c4376
AniKanayo <micanian4real@gmail.com>
2022-09-08 17:50:47 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,16 @@
-# !/ bin / bash
+#!/bin/bash
 #include <stdio.h>
 
 /** 
- *main - Entry point
- * Author: Michael
- * 
- * Return: Normally 0(Success)
- */
+*main - Entry point
+* Author: Michael
+* 
+* Return: Normally 0(Success)
+*/
 int main(void)
 {
 	char string[70] = " \"Programming is like bulding a multilingual puzzle\"";
+
 	puts(string);
 
 	/* Oh God please help me */
