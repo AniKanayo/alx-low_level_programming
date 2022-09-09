16833a2621466538a713a0cfff80bef0945eb9d7
AniKanayo <micanian4real@gmail.com>
2022-09-08 18:50:25 -0700

    size

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,19 +1,1 @@
-#include <stdio.h>
 
-/** 
-*main - Entry point
-* Author: Michael
-* 
-* Return: Normally 0(Success)
-*/
-int main(void)
-{
-	char string[70] = " \"Programming is like bulding a multilingual puzzle\"";
-
-	puts(string);
-
-	/* Oh God please help me */
-
-	puts("\"Programming is like building a multilingual puzzle\"");
-	return (0);
-}
