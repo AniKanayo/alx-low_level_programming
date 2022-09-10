ae458f5a70b7eaa752f5a666053c0d2a65ec9169
AniKanayo <micanian4real@gmail.com>
2022-09-09 19:56:31 -0700

    Inventing is a combination of brains and materials

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,26 +1,31 @@
 #include <stdio.h>
+
 /**
- * main - program compilation begins from main
- * Description: To print all possible combinations of two different numbers
- * author: Micky
- * Return: 0 means success
+ * main - print 00 to 99 with no duplicate digits or combos: no 11, no 10 (01)
+ * Author: michael
+ * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-	int a, b;
+	int ones;
+	int tens;
 
-	for (a = 48; a <=57; a++)
+	for (tens = '0'; tens <= '9'; tens++)
 	{
-		for (b = 49; b <= 57; b++)
+		for (ones = (tens + 1); ones <= '9'; ones++)
 		{
-			if ( a != b && b > a)
-                        {
-				putchar(a);
-				putchar(b);
+			putchar(tens);
+			putchar(ones);
+
+			if (tens != '8' || ones != '9')
+			{
 				putchar(',');
+				putchar(' ');
 			}
 		}
 	}
 	putchar('\n');
+
 	return (0);
 }
