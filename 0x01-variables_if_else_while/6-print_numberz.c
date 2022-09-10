58a6aea9aaf89acd9d198e03f18b0eba71f0d565
AniKanayo <micanian4real@gmail.com>
2022-09-09 17:44:46 -0700

    sixth print number

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - printing from zero to nine using putchar
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = '0' n <= '9' n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+
+	return (0);
+}
