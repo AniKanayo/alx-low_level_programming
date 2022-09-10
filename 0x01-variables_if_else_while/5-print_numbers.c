f6f073a814e403b1486290a36d371c83796cb3c9
AniKanayo <micanian4real@gmail.com>
2022-09-09 17:25:39 -0700

    fifth print of alpha

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * author: micky
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int micky;
+
+	for (micky = 0; micky < 10; micky++)
+	{
+		printf("%d", micky);
+	}
+	printf("\n");
+	return (0);
+}
