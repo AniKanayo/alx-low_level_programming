96d563f3274ef02d1c07416995067958d99a15e9
AniKanayo <micanian4real@gmail.com>
2022-09-09 18:15:27 -0700

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * autor: kanayo
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int micky;
+	char knayo;
+
+	for (micky = 48; micky < 58; micky++)
+		putchar(micky);
+	for (kanayo = 'a'; kanayo <= 'f'; kanayo++)
+		putchar(kanayo);
+	putchar('\n');
+	return (0);
+}
