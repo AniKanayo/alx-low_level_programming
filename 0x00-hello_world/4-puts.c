890db1a39be9dcf0adf23f171d3893b061d17a48
AniKanayo <micanian4real@gmail.com>
2022-09-12 13:32:49 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Author: micky
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char string[70] = "\"Programming is like building a multilingual puzzle\"";
+	
+	puts(string);
+
+	/* Tried printing it directly */
+
+	puts("\"Programming is like building a multilingual puzzle\"");
+	return (0);
+}
