fffc4434bd1047813f0f703a5b872329b220d9bd
AniKanayo <micanian4real@gmail.com>
2022-09-13 16:37:19 -0700

    main file

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -9,12 +9,12 @@
 int main(void)
 {
 
-	char a[] = "_putchar";
+	char a[] = '_putchar';
 	int b;
-	for (b = 0; b < 8; b++)
+	for (b = 0; b <= 7; b++)
 	{
 		_putchar(a[b]);
 	}
-	_putchar("\n");
+	_putchar('\n');
 	return (0);
 }
