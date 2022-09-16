c09d00f7ed92ea9c05d8f9e5fd454264becce99c
AniKanayo <micanian4real@gmail.com>
2022-09-15 17:00:44 -0700

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints number
+ * Return: void
+ */
+int mul(int a, int b)
+{
+	int a;
+	for (a = 48; a <= 57; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+}
