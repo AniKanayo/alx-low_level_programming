a035ad890675c7c0557893ce13989763e7e5229d
AniKanayo <micanian4real@gmail.com>
2022-09-16 16:44:59 -0700

    Numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,13 +8,20 @@ void more_numbers(void)
 {
 	int i, j;
 
-	for (i = 0; i < 11; i++)
+	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j < 15; j++)
 		{
-			_putchar(j);
+			if (j < 10)
+			{
+				_putchar(j + '0');
+			}
+			else
+				_putchar((j / 10) + '0');
+				_putchar((j % 10) + '0');
+			}
 		}
-	_putchar(i);
+		_putchar('\n');
 	}
-	_putchar('\n');
+	return;
 }
