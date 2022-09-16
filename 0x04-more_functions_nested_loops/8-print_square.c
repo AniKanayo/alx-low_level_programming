54645492d130ddda5bb44d140027ba9c94e73425
AniKanayo <micanian4real@gmail.com>
2022-09-15 19:36:57 -0700

    print square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_square - prints square
+ * @size: param
+ */
+
+void print_square(int size)
+{
+	int i, j, n;
+
+	for (i = 1; i <= n; i++)
+	{
+		for (j = 1; j <= n; j++)
+		{
+			_putchar(35);
+		}
+		_putchar('\n');
+	}
+}
