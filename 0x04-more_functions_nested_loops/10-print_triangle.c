12509e957f7a44a320b90474a23ef9bfcb93bedd
AniKanayo <micanian4real@gmail.com>
2022-09-15 21:07:07 -0700

    triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_triangle - To print triangle
+ * @i: first param
+ * @j: second param
+ * @n: third parameter
+ * Return: something
+ */
+void print_triangle(int size)
+{
+	int i, j, n;
+
+	for (i = 1; i <= n; i++)
+	{
+		for (j = n; j < n i++)
+		{
+			putchar (35);
+		}
+	putchar('\n');
+	returnn (0);
+}
