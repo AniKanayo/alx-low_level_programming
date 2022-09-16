f8893540b83a99488bd356f4795e55c45faf389f
AniKanayo <micanian4real@gmail.com>
2022-09-15 17:29:43 -0700

    most numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints number
+ * Return: void
+ */
+vvoid print_most_numbers(void)
+{
+	int b;
+
+	for (b = 48; b <= 57; b++)
+	{
+	if (b =! 2 || b =! 4)
+	{
+		_putchar(b);
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
