25486a2c923420d681548f4f778f268bf2befc80
AniKanayo <micanian4real@gmail.com>
2022-09-22 13:04:14 -0700

    Lowercase to uppercase

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,21 +1,20 @@
 #include "main.h"
 /**
  * string_toupper - a function that changes lowercase to uppercase
- * @x: pointer to array.
+ * @y: pointer to array.
  *
- * Return: 0 if successful
+ * Return: uppercase when successful
  */
-char *string_toupper(char *x)
+char *string_toupper(char *y)
 {
-	char x;
-	int i, dif = 'a' - 'A';
+	int i; /* i represents the length */
 
 	i = 0;
-	while (x[i])
+	while (y[i] != '\0')
 	{
-		if ((x[i] >= 'a') && (x[i] <= 'z'))
-			x[i] -= dif; /*convert to uppercase letters */
-		++i;
+		if ((y[i] >= 97) && (y[i] <= 122))
+			y[i] = y[i] - 32; /*convert to uppercase letters */
+		i++;
 	}
-	return (0);
+	return (y);
 }
