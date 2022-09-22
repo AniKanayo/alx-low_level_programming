afa80cbf4a632e1b7c946c3fa7b6e79a949fc1da
AniKanayo <micanian4real@gmail.com>
2022-09-22 10:55:11 -0700

    string copying

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,10 +12,20 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	for (i = 0; i <= dest[n]; i++)
+	i = 0;
+	while (i < n && src[i] != '\0')
 	{
-		dest[i] = src[i];
+	dest[i] = src[i];
+	i++;
 	}
+
+	while (i < n)
+	{
 	dest[i] = '\0';
-	return (0);
+	i++;
+	}
+
+	return (dest);
 }
+
+
