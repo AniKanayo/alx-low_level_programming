cbd1a61b3f5d383375c67fe2e88b56a6b41accf3
AniKanayo <micanian4real@gmail.com>
2022-09-22 12:46:10 +0000

    first concatenation

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strcat - a function that concanates two strings
+ * @dest: a pointer to destination string
+ * @src: a pointer to source string
+ *
+ * Return: return pointer to destination string.
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i, j; /* i holds dest length and j holds the length of the src */
+
+	i = j = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	while ((dest[i] = src[j++) != '\0')
+	{
+		dest[i] = src[j];
+		j++;
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
