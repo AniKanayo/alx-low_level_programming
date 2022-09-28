a052fafee29f2bb4a82aeeecfea9232bb8fd1a95
AniKanayo <micanian4real@gmail.com>
2022-09-28 14:20:09 -0700

    factorial in recursion

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -6,7 +6,7 @@
  */
 int factorial(int n)
 {
-	if ( n < 0)
+	if (n < 0)
 	{
 		return (-1);
 	}
@@ -16,6 +16,6 @@ int factorial(int n)
 	}
 	else
 	{
-		return (n * (factorial (n-1)));
+		return (n * (factorial(n - 1)));
 	}
 }
