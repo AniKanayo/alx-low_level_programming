d7a1d18215bc067de13a1db803eb5412285b0cbc
AniKanayo <micanian4real@gmail.com>
2022-09-30 14:08:39 -0700

    what is my name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - to print its own name and a newline character
+ * @argc: argument counter
+ * @argv: array pointing to arguements
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	argc = 0;
+	
+	printf("%s\n", argv[argc]);
+	return (0);
+}
