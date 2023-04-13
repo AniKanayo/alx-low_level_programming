373904b42d1d4edda4d795ce190ca27a4b225aa7
AniKanayo <micanian4real@gmail.com>
2023-04-13 21:54:51 +0000

    C - Search Algorithms

--- 0x1E-search_algorithms/105-jump_list.c
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -20,20 +20,20 @@ listint_t *jump_list(listint_t *list, size_t size, int value)
 	{
 		low = list;
 		high = list;
-		while (high->next != NULL && high->index < size && high->m < value)
+		while (high->next != NULL && high->index < size && high->n < value)
 		{
 			low = high;
 			limit += sqrt(size);
 			while (high->index < limit && high->next != NULL)
 				high = high->next;
-			printf("Value checked at index [%lu] = [%d]\n", high->index, high->m);
+			printf("Value checked at index [%lu] = [%d]\n", high->index, high->n);
 		}
 		printf("Value found between indexes [%lu] and [%lu]\n",
 		       low->index, high->index);
 		while (low != NULL && low->index < size && low->index <= high->index)
 		{
-			printf("Value checked at index [%lu] = [%d]\n", low->index, low->m);
-			if (low->m == value)
+			printf("Value checked at index [%lu] = [%d]\n", low->index, low->n);
+			if (low->n == value)
 				return (low);
 			low = low->next;
 		}
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -7,7 +7,7 @@
 
 /**
  * struct listint_s - singly linked list
- * @m: Integer
+ * @n: Integer
  * @index: Index of the node in the list
  * @next: Pointer to the next node
  *
@@ -16,7 +16,7 @@
  */
 typedef struct listint_s
 {
-	int m;
+	int n;
 	size_t index;
 	struct listint_s *next;
 } listint_t;
